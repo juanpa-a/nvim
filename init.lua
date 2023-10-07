6a400db52254d5c8602f47e9d44e8092839ac48a
Juan Pablo Ascencio <jpabloascencioglez@gmail.com>
2023-10-06 23:52:11 -0600

    removed comments

--- init.lua
+++ init.lua
@@ -4,12 +4,3 @@ require("juanpa.lazy")
 require("juanpa.after")
 
 vim.cmd.colorscheme "catppuccin-frappe"
-
-
-
-
-
-
-
--- -- The line beneath this is called `modeline`. See `:help modeline`
--- -- vim: ts=2 sts=2 sw=2 et
