68e7d857be6c59eb535b1e3f9dfe3040c02dd4ee
Juan Pablo Ascencio <jpabloascencioglez@gmail.com>
2023-10-07 00:55:25 -0600

    format on save

--- lua/juanpa/after/lsp.lua
+++ lua/juanpa/after/lsp.lua
@@ -91,3 +91,10 @@ mason_lspconfig.setup_handlers {
     }
   end
 }
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  buffer = buffer,
+  callback = function()
+    vim.lsp.buf.format { async = false }
+  end
+})
